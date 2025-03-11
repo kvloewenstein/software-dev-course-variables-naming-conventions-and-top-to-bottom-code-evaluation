58282a593b3914e9346766841516f35d0d04a8b9
kvloewenstein <Kvloewenstein@yahoo.com>
2025-03-10 23:05:49 -0500

    Repo make variable names more clear

--- variables-naming-conventions-and-top-to-bottom-code-evaluation.js
+++ variables-naming-conventions-and-top-to-bottom-code-evaluation.js
@@ -24,9 +24,9 @@ Things to reflect on:
   
 */
 
-let a = "Alice";
-let b = 5;
-let c = 20;
-let d = a + " bought " + b + " items for $" + c + ".";
+let customerName = "Alice";
+let numberOfItems = 5;
+let totalCost = 20;
+let purchaseSummary = customerName + " bought " + numberOfItems + " items for $" + totalCost + ".";
 
-console.log(d);
+console.log(purchaseSummary);
